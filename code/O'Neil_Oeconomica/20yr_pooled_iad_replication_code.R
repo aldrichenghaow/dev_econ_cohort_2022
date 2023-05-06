223eff4522c139cb53fcad4081c24bfc0642e88f
aidanfoneil <aoneil@uchicago.edu>
2023-05-06 14:06:20 -0500

    Code and two tables
    Uploading my work to git

---
+++ code/O'Neil_Oeconomica/20yr_pooled_iad_replication_code.R
@@ -0,0 +1,43 @@
+library(estimatr)
+library(data.table)
+library(tidyverse)
+library(ggplot2)
+library(testthat)
+library(janitor)
+library(rprojroot)
+
+dat <- read.csv("/Users/aidanoneil/Documents/GitHub/dev_econ_cohort_2022/data/prelim/income_and_democracy/20yr.csv")
+
+# Freedom house index
+
+# add lagged variables
+dat <- dat %>%
+  dplyr::mutate(lagged_fhpolrigaug = lag(fhpolrigaug)) %>%
+  as.data.table
+dat <- dat %>%
+  dplyr::mutate(lagged_lrgdpch = lag(lrgdpch)) %>%
+  as.data.table
+
+# run and save regression
+col_1 <- lm(fhpolrigaug ~ lagged_fhpolrigaug + lagged_lrgdpch, data = dat[sample == 1])
+summary(col_1)
+
+# cluster standard errors
+summary(col_1, vcov = vcovCL(model, cluster = dat$code))
+
+# Polity index
+
+dat <- dat %>%
+  dplyr::mutate(lagged_polity4 = lag(polity4)) %>%
+  as.data.table
+dat <- dat %>%
+  dplyr::mutate(lagged_lrgdpch = lag(lrgdpch)) %>%
+  as.data.table
+
+# run and save regression
+col_1 <- lm(polity4 ~ lagged_polity4 + lagged_lrgdpch, data = dat[sample == 1])
+summary(col_1)
+
+# cluster standard errors
+summary(col_1, vcov = vcovCL(model, cluster = dat$code))
+
